2f2d36c78b76ee86abcdb91531fcb74248f324f4
Ansoumane Kande <liam-kande@Ansoumanes-MBP.home>
2017-10-16 03:48:53 -0400

    created 404 route, passed parameters and data to routes.

--- src/components/App.js
+++ src/components/App.js
@@ -7,16 +7,17 @@ import About from './About'
 import Teachers from './Teachers'
 import Courses from './Courses'
 import NotFound from './NotFound'
+import Featured from './Featured'
 
 const App = () => (
   <BrowserRouter>
   <div className="container">
     <Header />
-    
     <Switch>
     <Route exact path="/" component={Home} />
     <Route path="/about" render={() => <About title='About'/>} />
-    <Route path="/teachers" component={Teachers} />
+    <Route exact path="/teachers" component={Teachers} />
+    <Route path="/teachers/:topic/:name" component={Featured} /> 
     <Route path="/courses" component={Courses} />
     <Route component={NotFound}/>
     </Switch>
